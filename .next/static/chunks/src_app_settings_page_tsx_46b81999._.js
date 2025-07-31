4e4e12a9adb6f50eb78eee419d139d3341b24003
Germán Ruiz <32862195+elvendk@users.noreply.github.com>
2025-07-31 01:59:20 -0400

    Add .next/static/chunks/src_app_settings_page_tsx_46b81999._.js 1753941560378

---
+++ .next/static/chunks/src_app_settings_page_tsx_46b81999._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e1300676._.js",
+  "static/chunks/node_modules_20236686._.js"
+],
+    source: "dynamic"
+});
